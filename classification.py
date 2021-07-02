f19919d6b56324e5435fb8a7a807f1db15337a9b
Timofey <nikiforov.ta@edu.spbstu.ru>
2021-07-02 16:57:09 +0300

    Added the ability to view the reference image of the dog breed after classification + minor changes

--- classification.py
+++ classification.py
@@ -3,32 +3,27 @@ from keras.models import load_model, Sequential
 from keras.preprocessing.image import load_img, img_to_array
 
 # Словарь вида "номер" : "порода" для интерпретации вывода модели
-mapping = {0: 'affenpinscher', 1: 'afghan_hound', 2: 'african_hunting_dog', 3: 'airedale',
-           4: 'american_staffordshire_terrier', 5: 'appenzeller', 6: 'australian_terrier', 7: 'basenji', 8: 'basset',
-           9: 'beagle', 10: 'bedlington_terrier', 11: 'bernese_mountain_dog', 12: 'black-and-tan_coonhound',
-           13: 'blenheim_spaniel', 14: 'bloodhound', 15: 'bluetick', 16: 'border_collie', 17: 'border_terrier',
-           18: 'borzoi', 19: 'boston_bull', 20: 'bouvier_des_flandres', 21: 'boxer', 22: 'brabancon_griffon',
-           23: 'briard', 24: 'brittany_spaniel', 25: 'bull_mastiff', 26: 'cairn', 27: 'cardigan',
-           28: 'chesapeake_bay_retriever', 29: 'chihuahua', 30: 'chow', 31: 'clumber', 32: 'cocker_spaniel',
-           33: 'collie', 34: 'curly-coated_retriever', 35: 'dandie_dinmont', 36: 'dhole', 37: 'dingo', 38: 'doberman',
-           39: 'english_foxhound', 40: 'english_setter', 41: 'english_springer', 42: 'entlebucher', 43: 'eskimo_dog',
-           44: 'flat-coated_retriever', 45: 'french_bulldog', 46: 'german_shepherd', 47: 'german_short-haired_pointer',
-           48: 'giant_schnauzer', 49: 'golden_retriever', 50: 'gordon_setter', 51: 'great_dane', 52: 'great_pyrenees',
-           53: 'greater_swiss_mountain_dog', 54: 'groenendael', 55: 'ibizan_hound', 56: 'irish_setter',
-           57: 'irish_terrier', 58: 'irish_water_spaniel', 59: 'irish_wolfhound', 60: 'italian_greyhound',
-           61: 'japanese_spaniel', 62: 'keeshond', 63: 'kelpie', 64: 'kerry_blue_terrier', 65: 'komondor', 66: 'kuvasz',
-           67: 'labrador_retriever', 68: 'lakeland_terrier', 69: 'leonberg', 70: 'lhasa', 71: 'malamute',
-           72: 'malinois', 73: 'maltese_dog', 74: 'mexican_hairless', 75: 'miniature_pinscher', 76: 'miniature_poodle',
-           77: 'miniature_schnauzer', 78: 'newfoundland', 79: 'norfolk_terrier', 80: 'norwegian_elkhound',
-           81: 'norwich_terrier', 82: 'old_english_sheepdog', 83: 'otterhound', 84: 'papillon', 85: 'pekinese',
-           86: 'pembroke', 87: 'pomeranian', 88: 'pug', 89: 'redbone', 90: 'rhodesian_ridgeback', 91: 'rottweiler',
-           92: 'saint_bernard', 93: 'saluki', 94: 'samoyed', 95: 'schipperke', 96: 'scotch_terrier',
-           97: 'scottish_deerhound', 98: 'sealyham_terrier', 99: 'shetland_sheepdog', 100: 'shih-tzu',
-           101: 'siberian_husky', 102: 'silky_terrier', 103: 'soft-coated_wheaten_terrier',
-           104: 'staffordshire_bullterrier', 105: 'standard_poodle', 106: 'standard_schnauzer', 107: 'sussex_spaniel',
-           108: 'tibetan_mastiff', 109: 'tibetan_terrier', 110: 'toy_poodle', 111: 'toy_terrier', 112: 'vizsla',
-           113: 'walker_hound', 114: 'weimaraner', 115: 'welsh_springer_spaniel', 116: 'west_highland_white_terrier',
-           117: 'whippet', 118: 'wire-haired_fox_terrier', 119: 'yorkshire_terrier'}
+mapping = ['Affenpinscher', 'Afghan Hound', 'African Hunting Dog', 'Airedale', 'American Staffordshire Terrier',
+           'Appenzeller', 'Australian Terrier', 'Basenji', 'Basset', 'Beagle', 'Bedlington Terrier',
+           'Bernese Mountain Dog', 'Black-And-Tan Coonhound', 'Blenheim Spaniel', 'Bloodhound', 'Bluetick',
+           'Border Collie', 'Border Terrier', 'Borzoi', 'Boston Bull', 'Bouvier Des Flandres', 'Boxer',
+           'Brabancon Griffon', 'Briard', 'Brittany Spaniel', 'Bull Mastiff', 'Cairn', 'Cardigan',
+           'Chesapeake Bay Retriever', 'Chihuahua', 'Chow', 'Clumber', 'Cocker Spaniel', 'Collie',
+           'Curly-Coated Retriever', 'Dandie Dinmont', 'Dhole', 'Dingo', 'Doberman', 'English Foxhound',
+           'English Setter', 'English Springer', 'Entlebucher', 'Eskimo Dog', 'Flat-Coated Retriever', 'French Bulldog',
+           'German Shepherd', 'German Short-Haired Pointer', 'Giant Schnauzer', 'Golden Retriever', 'Gordon Setter',
+           'Great Dane', 'Great Pyrenees', 'Greater Swiss Mountain Dog', 'Groenendael', 'Ibizan Hound', 'Irish Setter',
+           'Irish Terrier', 'Irish Water Spaniel', 'Irish Wolfhound', 'Italian Greyhound', 'Japanese Spaniel',
+           'Keeshond', 'Kelpie', 'Kerry Blue Terrier', 'Komondor', 'Kuvasz', 'Labrador Retriever', 'Lakeland Terrier',
+           'Leonberg', 'Lhasa', 'Malamute', 'Malinois', 'Maltese Dog', 'Mexican Hairless', 'Miniature Pinscher',
+           'Miniature Poodle', 'Miniature Schnauzer', 'Newfoundland', 'Norfolk Terrier', 'Norwegian Elkhound',
+           'Norwich Terrier', 'Old English Sheepdog', 'Otterhound', 'Papillon', 'Pekinese', 'Pembroke', 'Pomeranian',
+           'Pug', 'Redbone', 'Rhodesian Ridgeback', 'Rottweiler', 'Saint Bernard', 'Saluki', 'Samoyed', 'Schipperke',
+           'Scotch Terrier', 'Scottish Deerhound', 'Sealyham Terrier', 'Shetland Sheepdog', 'Shih-Tzu',
+           'Siberian Husky', 'Silky Terrier', 'Soft-Coated Wheaten Terrier', 'Staffordshire Bullterrier',
+           'Standard Poodle', 'Standard Schnauzer', 'Sussex Spaniel', 'Tibetan Mastiff', 'Tibetan Terrier',
+           'Toy Poodle', 'Toy Terrier', 'Vizsla', 'Walker Hound', 'Weimaraner', 'Welsh Springer Spaniel',
+           'West Highland White Terrier', 'Whippet', 'Wire-Haired Fox Terrier', 'Yorkshire Terrier']
 
 # Размер изображения для преобразования на вход модели
 IM_SIZE = 150
@@ -49,4 +44,4 @@ def classification(filename):
     # Нормализация изображения (переход от цвета в диапазоне [0,255] к диапазону [0,1])
     img_array = img_to_array(img) / 255
     # Словарь вида "порода" : "вероятность", полученный в результате работы модели
-    return dict(zip(mapping.values(), model.predict(np.expand_dims(img_array, axis=0))[0]))
+    return dict(zip(mapping, model.predict(np.expand_dims(img_array, axis=0))[0]))
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 
 from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QGridLayout
 
 from classification import classification, init_classification
 
@@ -25,6 +26,10 @@ class Window(QWidget):
         # Изначальные размеры отрисовываемого экрана
         self.width = 256
         self.height = 256
+
+        self.result = QWidget()
+        self.standard = QWidget()
+        self.breed_to_show = ""
 
         self.init_window()
 
@@ -50,26 +55,45 @@ class Window(QWidget):
         # Открытие окна выбора изображения
         fname = QFileDialog.getOpenFileName(self, 'Open File', './', "Image (*.png *.jpg *jpeg)")
         self.image_path = fname[0]
-        pixmap = QPixmap(self.image_path)
-        self.resize(pixmap.width(), pixmap.height())
+        pixmap = QPixmap(self.image_path).scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
         self.label.setPixmap(QPixmap(pixmap))
         # Активация кнопки вызова модели
         if not self.btn2.isEnabled():
             self.btn2.setEnabled(True)
+        self.result = QWidget()
 
     def print_class(self):
         # Получения отсортированного словаря вида "порода" : "вероятность"
         breeds = dict(sorted(classification(self.image_path).items(), key=lambda x: x[1], reverse=True))
-        # Информационное диалоговое окно
-        msg = QMessageBox()
-        msg.setWindowTitle("I think it is")
-        output = ""
+        self.result.setWindowTitle("I think it is")
+        self.result.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.result.setGeometry(600, 300, 256, 256)
+        msgbox = QGridLayout()
+        i = 0
         for k, v in breeds.items():
             if v > 0.01:
-                output += f"{100 * v:.2f}%\t\t{str.replace(k, '_', ' ').title()}\n"
-        msg.setText(output)
-        msg.setIcon(QMessageBox.Information)
-        msg.exec_()
+                msgbox.addWidget(QLabel(f"{100 * v:.2f}%"), i, 0)
+                btn_breed = QPushButton(k)
+                btn_breed.clicked.connect(self.show_match)
+                msgbox.addWidget(btn_breed, i, 1)
+                i += 1
+        self.result.setLayout(msgbox)
+        self.result.show()
+
+    def show_match(self):
+        self.standard = QWidget()
+        self.standard.setGeometry(850, 300, 220, 220)
+        breed = self.sender().text()
+        imgbox = QVBoxLayout()
+        breed_pixmap = QPixmap(f"./standard/{breed}.jpg")
+        breed_label = QLabel("")
+        breed_label.setPixmap(QPixmap(breed_pixmap))
+        imgbox.addWidget(breed_label)
+        self.standard.setLayout(imgbox)
+        self.standard.resize(breed_pixmap.width(), breed_pixmap.height())
+        self.standard.setWindowTitle(f"Breed: {breed}")
+        self.standard.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.standard.show()
 
 
 App = QApplication(sys.argv)
